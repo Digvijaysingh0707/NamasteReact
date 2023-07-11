2488760ff386b78ead39fa3a25a2c600bd4fbc02
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-07-12 02:54:48 +0530

    separating react file

---
+++ App.js
@@ -0,0 +1,5 @@
+
+// empty Object is used to attribute
+const heading = React.createElement("h1", { id: "hello" }, "Hello from React")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
