340b308baa458c819b285c1df7434723bab855db
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-07-12 22:30:31 +0530

    multiple childs

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 
 const parent = React.createElement(
   "div", { id: "parent" },
-  React.createElement("div", { id: "child" },
+  [React.createElement("div", { id: "child" },
     [React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag")
-  ]))
+    ]),
+  React.createElement("div", { id: "child2" },
+    [React.createElement("h1", {}, "I'm an h1 tag child2"),
+    React.createElement("h2", {}, "I'm an h2 tag child2")
+    ])
+  ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
