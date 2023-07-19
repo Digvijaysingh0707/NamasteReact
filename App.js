cd8d4ffe1eee261abab1853284e05126e8f83bb3
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-07-20 00:15:01 +0530

    changes

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement(
-  "div", { id: "parent" },
-  [React.createElement("div", { id: "child" },
-    [React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-  React.createElement("div", { id: "child2" },
-    [React.createElement("h1", {}, "I'm an h1 tag child2"),
-    React.createElement("h2", {}, "I'm an h2 tag child2")
-    ])
-  ])
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React")
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
-//this convert object into heading tag
-root.render(parent)``
+root.render(heading )
